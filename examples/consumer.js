9b0b4d4e2f07b9a37636df912bd4f0d01c89fb28
Samuel Gabel <smlgbl@gmail.com>
2014-04-09 22:39:48 +0200

    Add examples

---
+++ examples/consumer.js
@@ -0,0 +1,17 @@
+var csmr = require('../index');
+
+var conf = {host: "bx75", port: 5672};
+csmr.configure(conf, function() {
+  console.log("Ready.");
+  csmr.registerFn("task_queue", function(msg, cb) {
+    if(!!msg.count) {
+      console.log("Msg: %d", msg.count);
+      if(msg.count % 1000 === 1) {
+        csmr.sendMsg("statusUpdate", "Thousand messages, Yay!");
+      }
+    } else {
+      console.log("Msg: " + JSON.stringify(msg, null, 2));
+    }
+    cb();
+  });
+});
---
+++ examples/publisher.js
@@ -0,0 +1,15 @@
+var pub = require('../index');
+
+var data = {
+  count: 0,
+  raw: {xyz: 'mmmmmmmmmmmmmmmmmmmmmmmmmmm', bbb: 'lllllllllllllllllllllllllllllllllll'},
+  abc: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+};
+
+pub.configure({ host: "bx75", port: 5672 }, function() {
+  setInterval(function() {
+    data.count++;
+    pub.sendMsg("task_queue", data);
+    console.log(JSON.stringify(data, null, 2));
+  }, 1);
+});
